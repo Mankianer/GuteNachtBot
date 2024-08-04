f3af425d50f07ae168e95da2a8bb0ad8fe4f7b3f
Mankianer <mankianer@gmail.com>
2024-08-04 14:15:56 +0200

    init

---
+++ src/main/java/de/mankianer/gutenachtbot/GuteNachtBotApplication.java
@@ -0,0 +1,13 @@
+package de.mankianer.gutenachtbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuteNachtBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuteNachtBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mankianer/gutenachtbot/GuteNachtBotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mankianer.gutenachtbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuteNachtBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
